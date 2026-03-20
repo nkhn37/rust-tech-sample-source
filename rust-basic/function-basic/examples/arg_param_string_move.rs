9b19f1d9bb2a7968a107896854087a35678bc413
nkhn37 <nkhn37.github@gmail.com>
2026-03-20 13:57:37 +0900

    関数の基本に関するサンプルプログラム修正
    - 所有権が移動する場合は、コンパイルエラーとなるので、その旨を記載してコメントアウトに修正

--- rust-basic/function-basic/examples/arg_param_string_move.rs
+++ rust-basic/function-basic/examples/arg_param_string_move.rs
@@ -7,5 +7,7 @@ fn main() {
     // 所有権の移動の確認
     let s1 = String::from("Hello World");
     print_str_move(s1);
-    println!("呼び出し元のs1: {}", s1);
+
+    // 呼び出し元のs1は所有権が移動しているため、使用できない
+    // println!("呼び出し元のs1: {}", s1);
 }
